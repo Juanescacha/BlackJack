e27717a5623a61c7243dd12738d1d9f0a06c90f1
camiloGonzalez96 <48777609+camiloGonzalez96@users.noreply.github.com>
2019-06-09 16:33:31 -0500

    jugador
    se cambia la estructura de mano

--- src/Jugador.java
+++ src/Jugador.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 /**la clase Jugador se encarga de manejar las 2 cartas con las que se juegan, tambien administra el dinero
  * del jugador y maneja los metodos de interaccion con el usuario
  */
@@ -7,9 +9,8 @@ public class Jugador {
 	//se instancia el dinero con el que inicia el jugador para apostar
 	int dineroJugador = 1000000;
 	
-	//se declara la mano de tamaño indefi
-	Carta mano[] = {(Crupier.cartaJ1, cartaJ2};
-	
+	//se declara la mano de tamaño indefido
+	ArrayList<Carta> mano = new ArrayList<Carta>();
 
 	public Jugador() {
 		// TODO Auto-generated constructor stub
